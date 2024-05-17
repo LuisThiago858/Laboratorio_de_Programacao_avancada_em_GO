415e0b5bc6df147dfa53dd5116f244fb46c26224
LuisThiago858 <luisrabello.ltsr@gmail.com>
2024-05-16 20:30:15 -0400

    Organizacao dos Trabalhos

---
+++ criacaoDeGoRotinas.go
@@ -0,0 +1,26 @@
+/*
+Exercício: Criação de Gorotinas
+Crie duas aplicação implementadas usando a linguagem Go para um mesmo
+problema, sendo que as implementações serão uma sequencial e outra
+ paralela.
+
+1. Escolha uma aplicação que resolva qualquer problema, que possa ter
+uma opção de solução sequencial e uma opção paralela. Por exemplo
+soma dos elementos de um vetor de 10.000 posições.
+
+2. Faça a implementação sequencial e contabilize o tempo de execução
+da solução no início e no final da execução.
+
+3. Faça a implementação paralela e contabilize o tempo de execução da
+solução no início e no final da execução.
+
+4. Compare os tempos e comente sobre as soluções.
+
+OBSERVAÇÕES:
+1) Pode pegar idéias e trechos de códigos da internet, mas tem que
+comentar de onde copiou o trecho, além disso tem que acrerscentar
+novos trechos de codigo para não configurar plágio.
+2) Comente todas as funções no programa.
+3) Enviar o codigo fonte e um exemplo da execução.
+4) Coloque no codigo o nome dos membros da equipe
+*/
---
+++ produtor_consumidor/produtorConsumidor.go
@@ -0,0 +1,37 @@
+/*
+Exercício: Produtor/Cosumidor: Concorrencia usando Canais
+Assuma que um produtor vai ao depósito armazenar as caixas que vai
+ produzindo. Da mesma forma, um consumidor vai ao depósito para retirar
+  caixas que vai consumir.
+
+As seguintes atividades devem ser feitas:
+
+1) Implemente uma função Produtor que coloque um item de dado no canal,
+o canal inicialmente esta vazio; Não é possível armazenar nenhuma item
+caso a capacidade máxima já tenha sido atingida. Da mesma forma,
+não é possível retirar nenhum item, caso o canal esteja vazio.
+
+2) A função  Produtor deve funcionar como uma gorotina independente e
+que armazenar itens no canal.
+
+3) A função  Consumidor deve funcionar como uma gorotina independente
+e que retira itens do canal.
+
+4) Implemente uma função extra para limpar o canal, removendo todos
+os itens e fechar o mesmo.
+
+5) Implemente uma função extra para preencher  todo o canal,
+causando a suspensão da inserção. Ela deve ser usada para finalizar
+a produção e fechar o canal.
+
+
+OBSERVAÇÕES:
+1) Pode pegar idéias e trechos de códigos da internet, mas tem que
+comentar de onde copiou o trecho, além disso tem que acrerscentar novos
+trechos de codigo para não configurar plágio.
+2) Comente todas as funções no programa.
+3) Enviar o codigo fonte e um exemplo da execução.
+4) Coloque no codigo o nome dos membros da equipe.
+
+
+*/
