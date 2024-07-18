032f4d23fcf8c071178abae651317fc677e4b588
LuisThiago858 <luisrabello.ltsr@gmail.com>
2024-07-18 13:46:22 -0400

    Web server servindo conteúdo estático e dinâmico

--- 8.Web-server/main.go
+++ 8.Web-server/main.go
@@ -11,27 +11,31 @@ import (
 	"strings"
 )
 
+// serve a pagina estatica quando a porta 8080 e aberta
 func caminhoPaginaEstatica(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/index.html")
 }
 
+// serve paginas dinamicas de acordo com a chave e valor passadas. Exemplo localhost:8080/info?key=goroutine
 func paginaDinamica(w http.ResponseWriter, r *http.Request) {
 
-	key := r.URL.Query().Get("key")
+	key := r.URL.Query().Get("key") // pega a chave da URL
 	response := ""
 
+	//conteudo dinamico mostrado de acordo com o valor passado
 	data := map[string]string{
 		"golang":    "Golang é uma linguagem de programação criada pelo Google.",
 		"goroutine": "Goroutine é uma função executada concorrentemente em Go.",
 		"channel":   "Channel é um mecanismo de comunicação entre goroutines em Go.",
 	}
-
+	//caso o valor passado seja invalido o sistema mostra alguma opções validas
 	if value, ok := data[strings.ToLower(key)]; ok {
 		response = value
 	} else {
 		response = "Chave não encontrada. Tente 'golang', 'goroutine' ou 'channel"
 	}
 
+	//template basico das paginas dinamicas
 	tmpl, _ := template.New("response").Parse(`
 	<!DOCTYPE html>
 	<html>
@@ -49,9 +53,13 @@ func paginaDinamica(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//passa a pagina estatica assim que o sistema e iniciado
 	http.HandleFunc("/", caminhoPaginaEstatica)
+	//de acordo com o valor da chave passado após info mostra o conteudo dinamico
 	http.HandleFunc("/info", paginaDinamica)
 
+	//mostra onde a porta foi aberta
 	fmt.Println("Servidor aberto na porta 8080")
+	//abre a porta
 	http.ListenAndServe(":8080", nil)
 }
